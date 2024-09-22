84f93ac14284e76612dac86e1445bf48fe397b1c
KartikAg13 <23803013@mail.jiit.ac.in>
2024-09-22 20:21:37 +0530

    add recommendations

---
+++ config/Database.php
@@ -0,0 +1,18 @@
+<?php
+    class Database {
+        private $hostname = 'localhost';
+        private $username = 'root';
+        private $password = '';
+        private $database = 'recruitit';
+
+        protected function connect() {
+            try {
+                $connection = mysqli_connect($this->hostname, $this->username, $this->password, $this->database);
+                return $connection;
+            }
+            catch(mysqli_sql_exception $e) {
+                die("Connection Failed!" . $e->getMessage());
+            }
+        }
+    };
+?>
