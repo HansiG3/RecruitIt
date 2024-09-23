45412b24458c7c57e29eac1f27d27514bcddbc69
KartikAg13 <23803013@mail.jiit.ac.in>
2024-09-23 23:30:02 +0530

    start aboutus

---
+++ src/views/aboutus.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="This is a page about us">
+	<title>About Us</title>
+	<link rel="stylesheet" href="../../public/css/aboutus.css">
+	<link rel="stylesheet" href="../../public/css/font.css">
+	<link rel="stylesheet" href="../../public/css/navigation.css">
+</head>
+<body>
+	<main class="main_background noto-sans-warang-citi-regular">
+		
+		<?php include("navigation.html"); ?>
+
+		<div class="aboutus_main_content">
+			<h1 class="aboutus_heading"></h1>
+			<p class="aboutus_paragraph"></p>
+		</div>
+
+		<div class="aboutus_team">
+			<div class="aboutus_picture"></div>
+			<div class="aboutus_picture"></div>
+			<div class="aboutus_picture"></div>
+			<div class="aboutus_picture"></div>
+		</div>
+	</main>
+</body>
+</html>
