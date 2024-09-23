aa39cfcd14cf8353dabb6863ced06a3d75351bf8
HansiG3 <hansigupta3@gmail.com>
2024-09-23 22:07:34 +0530

    signup form validation

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,16 @@
 //make sure to create functions for everything and then simply call them
 function validateSignup() {
     let name=document.getElementById("name").value;
+    let name_error=document.getElementById("forname");
     let pass=document.getElementById("pwd").value;
+    let pass_error=document.getElementById("forpwd");
     let email=document.getElementById("email").value;
+    let email_error=document.getElementById("foremail")
     let contact=document.getElementById("contact").value;
+    let contact_error=document.getElementById("forcontact");
     let isvalid=true;
     if(name.trim()===""){
-        alert("Name Cannot Be Empty");
+        name_error.innerHTML="Name must not be empty";
         isvalid=false;
     }
     const specialChars = "!@#$%^&*()_+[]{}|;:',.<>?/`~";
@@ -16,23 +20,29 @@ function validateSignup() {
             count++;
         }
     }
-    if(count!==1){
-        isvalid=false;
-        alert("Must Contain atleast one Special Character");
+    let passerror="";
+    if(count!=1){
+        passerror+="Password Should Contain Atleast One Special Character ";
     }
-    if(pass.length<6){
-        alert("Password Length Should Not Be Less Than 6 Characters");
+    if(pass.length<8||pass.length>13){
+        passerror+="Password Length Should Be In Between 8 And 13 Characters";
+    }
+    if(passerror!=""){
+        pass_error.innerHTML=passerror;
         isvalid=false;
     }
+    
     const emailPattern = /\S+@\S+\.\S+/;
 
     if (!emailPattern.test(email)) {
-        alert("Please Enter Valid Email")
+        email_error.innerHTML="Please Enter A Valid Email";
         isvalid=false;
     }
-    if(contact.length!=10){
-        alert("Phone Number Must Be Of 10 Digits");
-        isvalid=false;
+    if(contact.length!=0){
+        if(contact.length!=10){
+            contact_error.innerHTML="Contact Number Must Be Of 10 Digits";
+            isvalid=false;
+        }
     }
     return isvalid;
 }
