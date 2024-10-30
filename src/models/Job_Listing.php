5f9f434888720f237f34399c9e85d738981a869d
KartikAg13 <23803013@mail.jiit.ac.in>
2024-10-30 17:33:50 +0530

    add automatic box generation

--- src/models/Job_Listing.php
+++ src/models/Job_Listing.php
@@ -41,5 +41,56 @@ class Job_Listing extends Employer
         mysqli_close($connection);
         return $job_id;
     }
+
+    public function displayJobListings()
+    {
+        $connection = parent::connect();
+        $query = "SELECT * FROM job_listing";
+        $result = mysqli_query($connection, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $posting_date = date("d M, Y", strtotime($row["posting_date"]));
+            $title = htmlspecialchars($row["title"]);
+            $job_type = ucfirst(str_replace("_", " ", $row["job_type"]));
+            $salary = number_format($row["salary"], 2);
+            $location = htmlspecialchars($row["location"]);
+
+            echo '
+            <div class="job-main noto-sans-warang-citi-regular">
+                <div class="job-card">
+                    <div class="job-header">
+                        <p class="posting">' .
+                $posting_date .
+                '</p>
+                    </div>
+                    <div class="company-info">
+                        <div class="job-info">
+                            <h2 class="job-title">' .
+                $title .
+                '</h2>
+                        </div>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" alt="Amazon Logo" class="company-logo" />
+                    </div>
+                    <div class="job-tags">
+                        <span>' .
+                $job_type .
+                '</span>
+                    </div>
+                </div>
+                <div class="job-footer">
+                    <div class="salary-place">
+                        <p class="rate">$' .
+                $salary .
+                '/hr</p>
+                        <p>' .
+                $location .
+                '</p>
+                    </div>
+                    <button class="details-btn">Details</button>
+                </div>
+            </div>';
+        }
+
+        mysqli_close($connection);
+    }
 }
 ?>
